68fb030392feee9fe7657c7e24130543f3e59685
Mert Efe Cerit <51813597+mertefecerit@users.noreply.github.com>
2022-06-15 14:09:40 +0300

    Change to SFC
    NEW FEATURE : dynamic dropdown emitable menu prop

--- Datatable.vue
+++ Datatable.vue
@@ -18,7 +18,7 @@
                 <thead class="bg-gray-300 text-gray-700">
                 <tr>
                     <th v-if="processColumn"></th>
-                    <th :class="{'hidden':hiddenColumns.includes(columnName.real)}" class="p-2" v-for="columnName in columnNames">
+                    <th class="p-2" :class="{'hidden':hiddenColumns.includes(columnName.real)}" v-for="columnName in columnNames">
                         <div class="flex gap-2 items-center justify-center">
                             <i @click.prevent="hiddenColumns.push(columnName.real)" class="fas fa-eye-slash hover:text-white cursor-pointer"></i>
                             <span @click="sortRecords(columnName.real)" class="hover:text-white cursor-pointer">
@@ -37,8 +37,9 @@
                             <span class="inline-block p-2 rounded bg-gray-500 hover:bg-gray-600 text-white"><i class="fas fa-list"></i></span>
                             <template v-slot:items>
                                 <ul class="text-left">
-                                    <li><a @click.prevent="$emit('onEdit', record)" href="#" class="text-sky-500 hover:text-sky-600 inline-block p-2 w-full hover:bg-gray-100 rounded"><i class="fas fa-edit mr-2"></i>Düzenle</a></li>
-                                    <li><a @click.prevent="$emit('onDelete', record)" href="#" class="text-red-500 hover:text-red-600 inline-block p-2 w-full hover:bg-gray-100 rounded"><i class="fas fa-trash mr-2"></i>Sil</a></li>
+                                    <li v-for="item in menuItems">
+                                        <a @click.prevent="$emit(item.emitName, record)" href="#" :class="item.classes"><i :class="item.icon"></i>{{ item.name }}</a>
+                                    </li>
                                 </ul>
                             </template>
                         </Dropdown>
@@ -65,19 +66,16 @@
     </div>
 </template>
 
-<script>
-import {computed, ref, watch, inject} from "vue";
-import Dropdown from "./Dropdown";
-import Alert from "./Alert";
+<script setup>
+    import {ref, watch, inject} from "vue";
+    import Dropdown from "./Dropdown";
+    import Alert from "./Alert";
 
-export default {
-    name: "Datatable",
-    emits:['datatableNewRecord','onEdit','onDelete'],
-    components:{
-        Dropdown,
-        Alert
-    },
-    props:{
+    const props = defineProps({
+        menuItems:{
+            default:[],
+            type:Array
+        },
         processColumn:{
             default: false,
             type: Boolean
@@ -98,123 +96,94 @@ export default {
             default:"records",
             type: String
         }
-    },
-    setup(props, context){
-        // Init comp. needed reactive variable section
-        const perPageRecordNumbers = ref([10,25,50,100]);
-        const perPageRecordNumber = ref(10);
-        const currentPage = ref(0);
-        const hiddenColumns = ref([]);
-        const sortDirection = ref();
-        const sortColumnName = ref();
-        const filterInputValue = ref();
-        const datatableRecords = ref([]);
-        const pageCount = ref(0);
-        let allRecords = [];
-        // Init comp. needed reactive variable section
+    });
+    const emit = defineEmits(['datatableNewRecord'])
 
+    // Init comp. needed reactive variable section
+    const perPageRecordNumbers = ref([10,25,50,100]);
+    const perPageRecordNumber = ref(10);
+    const currentPage = ref(0);
+    const hiddenColumns = ref([]);
+    const sortDirection = ref();
+    const sortColumnName = ref();
+    const filterInputValue = ref();
+    const datatableRecords = ref([]);
+    const pageCount = ref(0);
+    let allRecords = [];
+    // Init comp. needed reactive variable section
 
-        // Watch props.records and calculate data
-        watch(props, () => {
-            // get all records to new array
-            allRecords = [...props.records];
-            //calculate totalPage count
-            pageCount.value = Math.ceil(allRecords.length / perPageRecordNumber.value);
-            // set first page of datatable
-            datatableRecords.value = _.chunk(allRecords,perPageRecordNumber.value)[0];
-        },{deep:true})
-        // Watch props.records and calculate data
+    // Watch props.records and calculate data
+    watch(props, () => {
+        // get all records to new array
+        allRecords = [...props.records];
+        //calculate totalPage count
+        pageCount.value = Math.ceil(allRecords.length / perPageRecordNumber.value);
+        // set first page of datatable
+        datatableRecords.value = _.chunk(allRecords,perPageRecordNumber.value)[0];
+    },{deep:true})
+    // Watch props.records and calculate data
 
-
-
-        // PreviousPage or next button function
-        const previousPage = () => {
-            if(currentPage.value >= 1){
-                datatableRecords.value = _.chunk(allRecords,perPageRecordNumber.value)[currentPage.value - 1]
-                currentPage.value--
-            }
-
-        }
-        const nextPage = () => {
-            if ((currentPage.value + 1) < pageCount.value){
-                datatableRecords.value = _.chunk(allRecords,perPageRecordNumber.value)[currentPage.value + 1]
-                currentPage.value++
-            }
-        }
-        // PreviousPage or next button function
-
-
-
-        // Show pieces of records method
-        const chunkRecords = () => {
-            pageCount.value = Math.ceil(allRecords.length / perPageRecordNumber.value);
-            datatableRecords.value = _.chunk(allRecords,perPageRecordNumber.value)[0];
-        }
-        // Show pieces of records method
-
-
-        // Filter records inputbox method
-        const filterRecords = () => {
-            if(!filterInputValue.value){
-                allRecords = props.records;
-                pageCount.value = Math.ceil(props.records.length / perPageRecordNumber.value);
-                datatableRecords.value = _.chunk(props.records,perPageRecordNumber.value)[0];
-            }else{
-                allRecords = props.records.filter(item => {
-                    return JSON.stringify(item).toLocaleLowerCase().indexOf(filterInputValue.value.toLocaleLowerCase()) > -1;
-                });
-                pageCount.value = Math.ceil(allRecords.length / perPageRecordNumber.value);
-                datatableRecords.value = _.chunk(allRecords,perPageRecordNumber.value)[0];
-            }
-        }
-        // Filter records inputbox method
-
-
-
-        // Export excel file section
-        const xlsx = inject('xlsx');
-        const getExcel = () => {
-            const worksheet = xlsx.utils.json_to_sheet(allRecords);
-            const workbook = xlsx.utils.book_new();
-            xlsx.utils.book_append_sheet(workbook, worksheet, "Records");
-            xlsx.writeFile(workbook, props.exportFileName+'.xlsx');
-        }
-        // Export excel file section
-
-
-
-
-        const sortRecords = (columnName) => {
-            sortColumnName.value = columnName;
-            if (sortDirection.value === 'asc'){
-                sortDirection.value = 'desc';
-            }else{
-                sortDirection.value = 'asc'
-            }
-            allRecords = _.orderBy(allRecords,[columnName],[sortDirection.value]);
-            datatableRecords.value = _.chunk(allRecords,perPageRecordNumber.value)[0];
+    // PreviousPage or next button function
+    const previousPage = () => {
+        if(currentPage.value >= 1){
+            datatableRecords.value = _.chunk(allRecords,perPageRecordNumber.value)[currentPage.value - 1]
+            currentPage.value--
         }
 
-
-        return {
-            perPageRecordNumber,
-            datatableRecords,
-            perPageRecordNumbers,
-            chunkRecords,
-            filterRecords,
-            previousPage,
-            nextPage,
-            currentPage,
-            hiddenColumns,
-            getExcel,
-            sortDirection,
-            sortRecords,
-            sortColumnName,
-            filterInputValue,
-            pageCount
+    }
+    const nextPage = () => {
+        if ((currentPage.value + 1) < pageCount.value){
+            datatableRecords.value = _.chunk(allRecords,perPageRecordNumber.value)[currentPage.value + 1]
+            currentPage.value++
         }
     }
-}
+    // PreviousPage or next button function
+
+    // Show pieces of records method
+    const chunkRecords = () => {
+        pageCount.value = Math.ceil(allRecords.length / perPageRecordNumber.value);
+        datatableRecords.value = _.chunk(allRecords,perPageRecordNumber.value)[0];
+    }
+    // Show pieces of records method
+
+    // Filter records inputbox method
+    const filterRecords = () => {
+        if(!filterInputValue.value){
+            allRecords = props.records;
+            pageCount.value = Math.ceil(props.records.length / perPageRecordNumber.value);
+            datatableRecords.value = _.chunk(props.records,perPageRecordNumber.value)[0];
+        }else{
+            allRecords = props.records.filter(item => {
+                return JSON.stringify(item).toLocaleLowerCase().indexOf(filterInputValue.value.toLocaleLowerCase()) > -1;
+            });
+            pageCount.value = Math.ceil(allRecords.length / perPageRecordNumber.value);
+            datatableRecords.value = _.chunk(allRecords,perPageRecordNumber.value)[0];
+        }
+    }
+    // Filter records inputbox method
+
+    // Export excel file section
+    const xlsx = inject('xlsx');
+    const getExcel = () => {
+        const worksheet = xlsx.utils.json_to_sheet(allRecords);
+        const workbook = xlsx.utils.book_new();
+        xlsx.utils.book_append_sheet(workbook, worksheet, "Records");
+        xlsx.writeFile(workbook, props.exportFileName+'.xlsx');
+    }
+    // Export excel file section
+
+    // sortRecords function
+    const sortRecords = (columnName) => {
+        sortColumnName.value = columnName;
+        if (sortDirection.value === 'asc'){
+            sortDirection.value = 'desc';
+        }else{
+            sortDirection.value = 'asc'
+        }
+        allRecords = _.orderBy(allRecords,[columnName],[sortDirection.value]);
+        datatableRecords.value = _.chunk(allRecords,perPageRecordNumber.value)[0];
+    }
+    // sortRecords function
 </script>
 
 <style scoped>
