7713f46b9a78985b83d3ef7917232c87fa1ad20e
Mert Efe Cerit <mertefecerit@gmail.com>
2022-06-16 16:09:34 +0300

    FIX : RESP. Change Lang.

--- index.vue
+++ index.vue
@@ -1,26 +1,37 @@
 <template>
-    <div v-if="records.length > 0" class="overflow-auto bg-white p-2 rounded">
-        <div class="flex justify-between mb-2">
+    <div v-if="records.length > 0" class="bg-white p-2 rounded">
+        <div class="flex flex-col md:flex-row md:justify-between gap-2 mb-2">
             <div class="flex gap-2 items-center">
-                <button @click.prevent="$emit('onCreate')" class="p-2 bg-gray-500 hover:bg-gray-600 text-white px-4 py-2 focus:outline-0 rounded"><i class="fas fa-plus mr-2"></i>{{languages[language].newRecordButtonText}}</button>
-                <input v-model="filterInputValue" @keyup.prevent="filterRecords" type="text" :placeholder="languages[language].filterInputPlaceHolderText" class="p-2 bg-gray-100 border border-gray-200 focus:outline-0 rounded">
+                <button @click.prevent="$emit('onCreate')" class="p-2 bg-gray-500 hover:bg-gray-600 text-white px-4 py-2 focus:outline-0 rounded"><i class="fas fa-plus md:mr-2"></i><span class="hidden md:inline">{{languages[language].newRecordButtonText}}</span></button>
+                <input v-model="filterInputValue" @keyup.prevent="filterRecords" type="text" :placeholder="languages[language].filterInputPlaceHolderText" class="p-2 bg-gray-100 border border-gray-200 focus:outline-0 rounded flex-grow md:flex-grow-0">
             </div>
-            <div class="flex gap-2 items-center">
-                <button @click="hiddenColumns.length = 0" class="py-2 px-4 bg-gray-200 hover:bg-gray-300 text-gray-500 rounded"><i class="fas fa-eye mr-2"></i>{{languages[language].seeAllColumnsText}}</button>
+            <div class="flex justify-end gap-2 items-center">
                 <button @click.prevent="getExcel" class="py-2 px-4 bg-gray-200 hover:bg-gray-300 text-gray-500 focus:outline-0 rounded"><i class="fas fa-arrow-down-long"></i><i class="fas fa-file-excel mr-2"></i>Excel</button>
                 <select @change.prevent="chunkRecords" v-model="perPageRecordNumber" class="p-2 border bg-gray-100 focus:outline-0 rounded">
                     <option v-for="val in perPageRecordNumbers" :value="val" :key="val">{{val}}</option>
                 </select>
+                <div class="relative">
+                    <Dropdown classes="w-44 p-2 border right-0">
+                        <span class="inline-block p-2 rounded bg-gray-200 hover:bg-gray-300 text-gray-500"><i class="fas fa-eye"></i></span>
+                        <template v-slot:items>
+                            <ul class="flex flex-col gap-2">
+                                <li><a @click.prevent="hiddenColumns.length = 0" href="#" :class="{'bg-sky-500 text-white':hiddenColumns.length === 0}" class="inline-block p-2 w-full rounded"><i class="fas fa-eye mr-2"></i>{{languages[language].seeAllColumnsText}}</a></li>
+                                <li v-for="columnName in columnNames" :key="columnName">
+                                    <a @click.prevent="toggleHiddenColumn(columnName.real)" :class="{'bg-sky-500 text-white':!hiddenColumns.includes(columnName.real)}" href="#" class="inline-block p-2 w-full rounded">{{ columnName.name }}</a>
+                                </li>
+                            </ul>
+                        </template>
+                    </Dropdown>
+                </div>
             </div>
         </div>
-        <div class="rounded overflow-hidden border mb-2">
+        <div class="rounded overflow-auto border mb-2">
             <table class="table-auto w-full text-center">
                 <thead class="bg-gray-300 text-gray-700">
                 <tr>
                     <th v-if="processColumn"></th>
                     <th class="p-2" :class="{'hidden':hiddenColumns.includes(columnName.real)}" v-for="columnName in columnNames" :key="columnName">
                         <div class="flex gap-2 items-center justify-center">
-                            <i @click.prevent="hiddenColumns.push(columnName.real)" class="fas fa-eye-slash hover:text-white cursor-pointer"></i>
                             <span @click="sortRecords(columnName.real)" class="hover:text-white cursor-pointer">
                                 {{columnName.name}}
                                 <i v-if="sortDirection === 'asc' && sortColumnName === columnName.real" class="fas fa-sort-down ml-1"></i>
@@ -49,7 +60,7 @@
                 </tbody>
             </table>
         </div>
-        <div class="flex justify-between items-center">
+        <div class="flex flex-col-reverse flex-col md:flex-row md:justify-between gap-2 items-center">
             <span class="text-gray-500">{{languages[language].totalRecord}} : {{records.length}}</span>
             <div class="rounded overflow-hidden">
                 <button :disabled="currentPage === 0" @click.prevent="previousPage" class="p-2 bg-gray-500 text-white hover:bg-gray-600 transition-all disabled:cursor-not-allowed"><i class="fas fa-arrow-alt-circle-left mr-2"></i>{{languages[language].nextButtonText}}</button>
@@ -189,6 +200,16 @@ import {watchEffect, ref, inject} from "vue";
         datatableRecords.value = _.chunk(allRecords,perPageRecordNumber.value)[0];
     }
     // sortRecords function
+
+    // toggle hiddencolumn function
+    const toggleHiddenColumn = (columnName) => {
+        if (hiddenColumns.value.includes(columnName)){
+            hiddenColumns.value.splice(hiddenColumns.value.indexOf(columnName),1)
+        }else{
+            hiddenColumns.value.push(columnName)
+        }
+    }
+    // toggle hiddencolumn function
 </script>
 
 <style scoped>
