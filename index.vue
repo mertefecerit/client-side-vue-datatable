40fdea059167432ef6494c384f821507633c9b2b
Mert Efe Cerit <51813597+mertefecerit@users.noreply.github.com>
2022-06-16 08:19:49 +0300

    fix some design bug

--- index.vue
+++ index.vue
@@ -1,15 +1,15 @@
 <template>
-    <div v-if="records.length > 0" class="overflow-auto">
+    <div v-if="records.length > 0" class="overflow-auto bg-white p-2 rounded">
         <div class="flex justify-between mb-2">
             <div class="flex gap-2 items-center">
-                <button @click.prevent="$emit('datatableNewRecord')" class="p-2 bg-gray-500 hover:bg-gray-600 text-white px-4 py-2 focus:outline-0 rounded"><i class="fas fa-plus mr-2"></i>Yeni Kayıt</button>
+                <button @click.prevent="$emit('onCreate')" class="p-2 bg-gray-500 hover:bg-gray-600 text-white px-4 py-2 focus:outline-0 rounded"><i class="fas fa-plus mr-2"></i>Yeni Kayıt</button>
                 <input v-model="filterInputValue" @keyup.prevent="filterRecords" type="text" placeholder="Kayıt Ara" class="p-2 bg-gray-100 border border-gray-200 focus:outline-0 rounded">
             </div>
             <div class="flex gap-2 items-center">
                 <button @click="hiddenColumns.length = 0" class="py-2 px-4 bg-gray-200 hover:bg-gray-300 text-gray-500 rounded"><i class="fas fa-eye mr-2"></i>Tüm Kolonları Göster</button>
                 <button @click.prevent="getExcel" class="py-2 px-4 bg-gray-200 hover:bg-gray-300 text-gray-500 focus:outline-0 rounded"><i class="fas fa-arrow-down-long"></i><i class="fas fa-file-excel mr-2"></i>Excel</button>
                 <select @change.prevent="chunkRecords" v-model="perPageRecordNumber" class="p-2 border bg-gray-100 focus:outline-0 rounded">
-                    <option v-for="val in perPageRecordNumbers" :value="val">{{val}}</option>
+                    <option v-for="val in perPageRecordNumbers" :value="val" :key="val">{{val}}</option>
                 </select>
             </div>
         </div>
@@ -18,7 +18,7 @@
                 <thead class="bg-gray-300 text-gray-700">
                 <tr>
                     <th v-if="processColumn"></th>
-                    <th class="p-2" :class="{'hidden':hiddenColumns.includes(columnName.real)}" v-for="columnName in columnNames">
+                    <th class="p-2" :class="{'hidden':hiddenColumns.includes(columnName.real)}" v-for="columnName in columnNames" :key="columnName">
                         <div class="flex gap-2 items-center justify-center">
                             <i @click.prevent="hiddenColumns.push(columnName.real)" class="fas fa-eye-slash hover:text-white cursor-pointer"></i>
                             <span @click="sortRecords(columnName.real)" class="hover:text-white cursor-pointer">
@@ -31,20 +31,20 @@
                 </tr>
                 </thead>
                 <tbody class="divide-y divide-gray-100">
-                <tr v-for="(record, index) in datatableRecords" :class="{'bg-gray-50':index % 2 !== 0}">
+                <tr v-for="(record, index) in datatableRecords" :class="{'bg-gray-50':index % 2 !== 0}" :key="record">
                     <td v-if="processColumn" class="p-2 text-left">
                         <Dropdown classes="w-44 p-2 border">
                             <span class="inline-block p-2 rounded bg-gray-500 hover:bg-gray-600 text-white"><i class="fas fa-list"></i></span>
                             <template v-slot:items>
                                 <ul class="text-left">
-                                    <li v-for="item in menuItems">
+                                    <li v-for="item in menuItems" :key="item">
                                         <a @click.prevent="$emit(item.emitName, record)" href="#" :class="item.classes"><i :class="item.icon"></i>{{ item.name }}</a>
                                     </li>
                                 </ul>
                             </template>
                         </Dropdown>
                     </td>
-                    <td :class="{'hidden':hiddenColumns.includes(key)}" class="p-2" v-for="key in Object.keys(record)" v-show="!exceptColumns.includes(key)">{{record[key]}}</td>
+                    <td :class="{'hidden':hiddenColumns.includes(key)}" class="p-2" v-for="key in Object.keys(record)" v-show="!exceptColumns.includes(key)" :key="key">{{record[key]}}</td>
                 </tr>
                 </tbody>
             </table>
@@ -59,9 +59,9 @@
         </div>
     </div>
     <div v-else class="flex flex-col gap-4 items-center">
-        <button @click.prevent="$emit('datatableNewRecord')" class="p-2 bg-gray-500 hover:bg-gray-600 text-white px-4 py-2 focus:outline-0 rounded"><i class="fas fa-plus mr-2"></i>Yeni Kayıt</button>
+        <button @click.prevent="$emit('onCreate')" class="p-2 bg-gray-500 hover:bg-gray-600 text-white px-4 py-2 focus:outline-0 rounded"><i class="fas fa-plus mr-2"></i>Yeni Kayıt</button>
         <Alert>
-            <span>Henüz burada bir kayıt yok</span>
+            <span>No records here yet</span>
         </Alert>
     </div>
 </template>
